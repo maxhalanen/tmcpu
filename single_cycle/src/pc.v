9096e94dd539966fc2cc14c6d9929c6241324621
maxhalanen <maximilian.halanen@gmail.com>
2025-11-13 21:32:50 -0500

    Added program counter

---
+++ single_cycle/src/pc.v
@@ -0,0 +1,31 @@
+// program counter 
+
+module pc (
+    input clk,
+    input rst_n,
+    input [31:0] pc_in,
+    output reg [31:0] pc_out
+);
+
+always @(posedge clk or negedge rst_n) begin 
+    if (rst_n == 1'b00) begin
+        pc_out <= 32'b00;
+    end else begin
+        pc_out <= pc_in;
+    end
+end
+
+endmodule
+
+module inc_pc (
+    input  [31:0] pc_curr,
+    output [31:0] pc_next,
+);
+
+assign pc_next = 4 + pc_curr;
+
+endmodule
+
+
+
+
