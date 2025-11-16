583dcf1fdbd125e60f16f8283cc075b050552b83
Max Halanen <maximilian.halanen@gmail.com>
2025-11-16 16:52:19 -0500

    Tested out iverilog with basic blinky example

--- single_cycle/src/pc.v
+++ single_cycle/src/pc.v
@@ -7,23 +7,21 @@ module pc (
     output reg [31:0] pc_out
 );
 
-always @(posedge clk or negedge rst_n) begin 
-    if (rst_n == 1'b00) begin
-        pc_out <= 32'b00;
-    end else begin
-        pc_out <= pc_in;
+    always @(posedge clk or negedge rst_n) begin 
+        if (rst_n == 1'b0) begin
+            pc_out <= 32'b0;
+        end else begin
+            pc_out <= pc_in;
+        end
     end
-end
 
 endmodule
 
 module inc_pc (
-    input  [31:0] pc_curr,
-    output [31:0] pc_next,
+    input  [31:0] pc_in,
+    output [31:0] pc_out 
 );
-
-assign pc_next = 4 + pc_curr;
-
+    assign pc_out = 4 + pc_in;
 endmodule
 
 
