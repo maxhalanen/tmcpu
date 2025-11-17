095aba482cd24f681798098b9cbaf47dfe49d410
Max Halanen <maximilian.halanen@gmail.com>
2025-11-16 19:07:53 -0500

    Added program counter testbench

--- single_cycle/src/pcBench.v
+++ single_cycle/src/pcBench.v
@@ -1,14 +1,8 @@
-
-
-
-
 module bench();
     reg CLK;
     reg RST_N = 1;
     wire [31:0] NEXT_PC;
     wire [31:0] PC_OUT;
-    reg [31:0] NEXT_PC_REG;
-
 
     pc count (
         .clk(CLK),
@@ -22,26 +16,16 @@ module bench();
         .pc_out(NEXT_PC)
     );
 
-    always @(posedge CLK or negedge RST_N) begin 
-        if (!RST_N) begin
-            NEXT_PC_REG <= 32'b0;
-        end else begin
-            NEXT_PC_REG <= NEXT_PC;
-        end
-    end
-
-
     initial begin 
         CLK = 0;
         RST_N = 0;
-        #10 RST_N = 10;
+        #10 RST_N = 1;
         
         forever #5 CLK = ~CLK;
     end
 
     always @(posedge CLK) begin
-        $display("PC: %b  %b", PC_OUT, NEXT_PC_REG);
+        $display("PC: %h  %h", NEXT_PC, PC_OUT);
     end
-
 
 endmodule
