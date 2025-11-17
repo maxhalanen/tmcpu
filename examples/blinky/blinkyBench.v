d39e0bb4f6e69d9b5ccbde2f0b6760c42290de6e
Max Halanen <maximilian.halanen@gmail.com>
2025-11-16 19:07:53 -0500

    Added program counter testbench

--- examples/blinky/blinkyBench.v
+++ examples/blinky/blinkyBench.v
@@ -17,11 +17,11 @@ module bench();
    initial begin
       CLK = 0;
       forever begin
-		#1 CLK = ~CLK;
-		if(LEDS != prev_LEDS) begin
-			$display("LEDS = %b",LEDS);
-		end
-		prev_LEDS <= LEDS;
+         #1 CLK = ~CLK;
+         if(LEDS != prev_LEDS) begin
+            $display("LEDS = %b",LEDS);
+         end
+         prev_LEDS <= LEDS;
       end
    end
 endmodule   
